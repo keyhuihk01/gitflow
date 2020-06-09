545106eb8d6c53d66dd8d03d50250f385cbdc681
key.hui <keyhui@hk01.com>
2020-06-09 15:23:37 +0800

    chore: initial commit

---
+++ changelog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -el
+
+nvm install 10
+nvm use 10
+
+git reset --hard # remove the credential
+git config user.email 'builds@travis-ci.com' && \
+git config user.name 'Travis CI' && \
+npx standard-version --commit-all --message $'chore: %s\n\n[skip changelog]' --no-verify && \
+git push --verbose --no-verify --follow-tags origin HEAD:${TRAVIS_BRANCH}
