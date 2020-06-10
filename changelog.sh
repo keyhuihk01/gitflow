fdb4c531459f20525185e41d01d6d29224cd29bd
key.hui <keyhui@hk01.com>
2020-06-10 12:09:13 +0800

    ci: update

--- changelog.sh
+++ changelog.sh
@@ -7,4 +7,4 @@ git reset --hard # remove the credential
 git config user.email 'builds@travis-ci.com' && \
 git config user.name 'Travis CI' && \
 npx standard-version --commit-all --message $'chore: %s\n\n[skip changelog]' --no-verify && \
-git push --verbose --no-verify --follow-tags origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}
+git push --verbose --no-verify --follow-tags origin https://github.com/${TRAVIS_REPO_SLUG}
