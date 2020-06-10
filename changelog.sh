08b2e6c293d0533421b6f9f8e1a420bc1eb40252
key.hui <keyhui@hk01.com>
2020-06-10 16:21:24 +0800

    ci: update

--- changelog.sh
+++ changelog.sh
@@ -7,4 +7,4 @@ git reset --hard # remove the credential
 git config user.email 'builds@travis-ci.com' && \
 git config user.name 'Travis CI' && \
 npx standard-version --commit-all --message $'chore: %s\n\n[skip changelog]' --no-verify && \
-git push "https://${DEPLOY_KEY}@github.com/keyhuihk01/gitflow.git"
+git push "https://${DEPLOY_KEY}@github.com/keyhuihk01/gitflow.git" master
